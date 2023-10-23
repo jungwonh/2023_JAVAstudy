f1fbb349052e10041c08fa2d01172329c91d5d9d
oneii <hur030331@gmail.com>
2023-10-24 00:00:33 +0900

    z

---
+++ Week06_oneii/src/MethodReferenceLowerCase.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MethodReferenceLowerCase {
+	
+	public static void main(String[] args) {
+		List<String> listOfNames=Arrays.asList(new String[]
+				{"Apple", "Banana", "Cherry"});
+	}
+
+}
